61d776b505e6cf3f301263fbd4a4b4cb6748aa74
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-10 20:52:21 +0100

    starting on header

---
+++ src/components/header/hamburger-menu/hamburger-menu.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./hamburger-menu.css"
+
+function HamburgerMenu() {
+  return (
+    <div class="hamburger-wrapper">
+          <h3>
+              <span class="letter">o</span>
+              <span class="letter">p</span>
+          </h3>
+          <h3>
+          <span class="letter">e</span>
+          <span class="letter">n</span>
+          </h3>
+          
+    </div>
+  );
+}
+
+export default HamburgerMenu;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import "./header.css"
+import HamburgerMenu from "./hamburger-menu/hamburger-menu.js"
 
 function Header() {
   return (
-    <div>Header
+    <div class="header-wrapper">
+      <h3>logga</h3>
+      <nav><h3>home</h3>
+      <h3>menu</h3>
+      <h3>about</h3>
+      <h3>news</h3></nav>
+      <HamburgerMenu />
     </div>
   );
 }
