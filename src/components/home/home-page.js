2c75cbc176f6acc858b8c4f26720f393cecbc81b
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-15 23:20:42 +0100

    working on section 2 homepage

--- src/components/home/home-page.js
+++ src/components/home/home-page.js
@@ -16,8 +16,34 @@ function HomePage() {
               <span>to </span>
               <span>miri</span>
             </h1>
-            <h4>asian fusion restaurant<hr></hr></h4>
-            
+            <h4>
+              asian fusion restaurant<hr></hr>
+            </h4>
+          </div>
+          <div class="s2-wrapper">
+            <img src={mountain} alt="mountain"></img>
+            <div class="s2-text-flex">
+              <div class="s2-body-text">
+                <h4>
+                  a little bit about us<hr></hr>
+                </h4>
+                <h5>"For the people, food is heaven. ”
+                
+This is as true in tropical chili-loving Sichuan as among the hunters on the snow-capped peaks of the Himalayas. Likewise in the cumin-scented Muslim deserts on the Silk Road in Xinjiang or along the coasts with a seafood feast in the cities of Shanghai and Hong Kong.
+
+So let the journey begin!
+
+We want to recommend you to eat here with us in the same way as you do there! Which means that you order a bunch of dishes that you then eat together.
+We are more than happy to help you with recommendations on what should be eaten with what and how.
+
+It does not have to be more difficult than that.
+
+Warm welcome!</h5>
+              </div>
+              <div class="s2-heading">
+                <h2>hello</h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
