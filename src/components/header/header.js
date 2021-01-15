0892bb77384b079e7097b547530579591c29f23c
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-15 08:53:09 +0100

    nav works

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,26 @@
 import React from "react";
-import "./header.css"
-
+import "./header.css";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div class="header-wrapper">
       <h3 class="logo">logga</h3>
       <nav>
-      <h3>home</h3>
-      <h3>menu</h3>
-      <h3>about</h3>
-      <h3>news</h3>
+        <h3>
+          <NavLink to="/" exact>
+            home
+          </NavLink>
+        </h3>
+        <h3>
+          <NavLink to="/menu">menu</NavLink>
+        </h3>
+        <h3>
+          <NavLink to="/about">about</NavLink>
+        </h3>
+        <h3>
+          <NavLink to="/news">news</NavLink>
+        </h3>
       </nav>
     </div>
   );
