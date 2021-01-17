281cb0f96c0116c865d5213081db032ba1348c24
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-17 10:20:00 +0100

    section 5 and 6 homepage done

--- src/components/home/home-page.js
+++ src/components/home/home-page.js
@@ -73,18 +73,52 @@ function HomePage() {
         <div class="s4-wrapper">
           <img src={mountain} alt="mountain"></img>
           <div class="s4-text">
-            <div class="s4-heading"><h2>book a table
-            </h2></div>
-            <div class="s4-body-text"><h4>
-              call us on 071943670<hr></hr>
-            </h4>
-            <h5>
-              "For the people, food is heaven. ” This is as true in tropical
-              chili-loving Sichuan as among the hunters on the snow-capped peaks
-              of the Himalayas.
-            </h5></div>
+            <div class="s4-heading">
+              <h2>book a table</h2>
+            </div>
+            <div class="s4-body-text">
+              <h4>
+                call us on 071943670<hr></hr>
+              </h4>
+              <h5>
+                "For the people, food is heaven. ” This is as true in tropical
+                chili-loving Sichuan as among the hunters on the snow-capped
+                peaks of the Himalayas.
+              </h5>
+            </div>
           </div>
-         
+        </div>
+        <div class="s5-s6-background">
+          <div class="s5-wrapper">
+            <img src={mountain} alt="mountain"></img>
+            <h2 class="s5-heading">MENU</h2>
+
+            <div class="s5-body-text">
+              <h4>
+                call us on 071943670<hr></hr>
+              </h4>
+              <h5>
+                "For the people, food is heaven. ” This is as true in tropical
+                chili-loving Sichuan as among the hunters on the snow-capped
+                peaks of the Himalayas.
+              </h5>
+            </div>
+          </div>
+          <div class="s6-wrapper">
+            <img src={mountain} alt="mountain"></img>
+            <h2 class="s6-heading">LATEST NEWS</h2>
+
+            <div class="s6-body-text">
+              <h4>
+                call us on 071943670<hr></hr>
+              </h4>
+              <h5>
+                "For the people, food is heaven. ” This is as true in tropical
+                chili-loving Sichuan as among the hunters on the snow-capped
+                peaks of the Himalayas.
+              </h5>
+            </div>
+          </div>
         </div>
       </div>
     </div>
