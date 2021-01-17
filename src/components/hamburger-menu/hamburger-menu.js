db5bb4d4187bb36935d05122448ac3f2481ad1a7
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-17 18:48:16 +0100

    responsive

--- src/components/hamburger-menu/hamburger-menu.js
+++ src/components/hamburger-menu/hamburger-menu.js
@@ -1,21 +1,18 @@
 import React from "react";
-import "./hamburger-menu.css"
+import "./hamburger-menu.css";
 
 function HamburgerMenu() {
   return (
-    
     <div class="hamburger-wrapper">
-          <div>
-              <span class="letter">o</span>
-              <span class="letter">p</span>
-          </div>
-          <div>
-          <span class="letter">e</span>
-          <span class="letter">n</span>
-          </div>
-          
+      <div>
+        <span class="letter">o</span>
+        <span class="letter">p</span>
+      </div>
+      <div>
+        <span class="letter">e</span>
+        <span class="letter">n</span>
+      </div>
     </div>
-    
   );
 }
 
--- src/components/menu/menu-page.js
+++ src/components/menu/menu-page.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-
 function MenuPage() {
   return (
-    <div>Menupage
+    <div>
+      <div>Section one</div>
+      <div>Section two and three</div>
+      <div>Section four</div>
+      <div>Section five and six</div>
     </div>
   );
 }
