9bf75baa55a969c658a2ec8b26d69d146c85d854
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-17 21:42:01 +0100

    working on content footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,14 +7,34 @@ function Footer() {
     <div class="footer">
       <div class="footer-relative">
         <img src={mountain} alt="mountain"></img>
-        <div class="footer-text">
+        <h2>CONTACT</h2>
+          <div class="footer-text">
           <h4>
-            asian fusion restaurant<hr></hr>
+          Sankt Eriksgatan 70, 113 20 Stockholm
+          </h4>
+          <h4>
+          phone 040-121490
+          </h4>
+          <h4>
+          OPENING HOURS: Monday - Sunday 11:30 - 22:00
           </h4>
         </div>
+        <div class="footer-social">
+          <hr></hr>
+          </div>
+          <div class="footer-logo">LOGO</div>
       </div>
+      
     </div>
   );
 }
 
 export default Footer;
+
+/* 
+import { ReactComponent as IconName } from "../path-icon.svg";
+<IconName /> 
+
+
+<hr></hr>
+*/
