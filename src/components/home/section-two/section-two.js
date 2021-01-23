35945d991db4ac18f01289eff1dc7915c5ae55b5
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-23 20:18:04 +0100

    content section 1&2 home

--- src/components/home/section-two/section-two.js
+++ src/components/home/section-two/section-two.js
@@ -4,33 +4,30 @@ import mountain from "../../../media/mountain.jpg";
 
 function SectionTwo() {
   return (
-     
     <div class="hp-s2-relative">
-    <img src={mountain} alt="mountain"></img>
-    <div class="hp-s2-text">
-      <div class="hp-s2-text-body">
-        <h3>
-          a little bit about us<hr></hr>
-        </h3>
-        <h5>
-          "For the people, food is heaven. ” This is as true in tropical
-          chili-loving Sichuan as among the hunters on the snow-capped peaks
-          of the Himalayas. Likewise in the cumin-scented Muslim deserts on
-          the Silk Road in Xinjiang or along the coasts with a seafood feast
-          in the cities of Shanghai and Hong Kong. So let the journey begin!
-          We want to recommend you to eat here with us in the same way as
-          you do there! Which means that you order a bunch of dishes that
-          you then eat together. We are more than happy to help you with
-          recommendations on what should be eaten with what and how. It does
-          not have to be more difficult than that. Warm welcome!
-        </h5>
-      </div>
-      <div class="hp-s2-heading">
-        <h2>hello</h2>
+      <img src={mountain} alt="mountain"></img>
+      <div class="hp-s2-text">
+        <div class="hp-s2-text-body">
+          <h3>
+            "For the people, food is heaven"<hr></hr>
+          </h3>
+          <h5>
+            This is as true in tropical chili-loving Sichuan as among the
+            hunters on the snow-capped peaks of the Himalayas.<br></br> Likewise
+            in the cumin-scented Muslim deserts on the Silk Road in Xinjiang. <br></br>
+            So let the journey begin!<br></br><br></br>
+            Miri serves you tasty, healthy dishes that are served quickly,
+            freshly and simply in a warm and playful environment. The basis of
+            our kitchen is our interpretation of street food from the asian
+            cusine. We are open for all, days and nights. Come as you are.<br></br>
+            <strong>Warm welcome!</strong>
+          </h5>
+        </div>
+        <div class="hp-s2-heading">
+          <h2>who <br></br>we are</h2>
+        </div>
       </div>
     </div>
-  </div>
-  
   );
 }
 
