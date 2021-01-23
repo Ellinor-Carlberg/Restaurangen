38f67b06a685896009ab5d7d8d0da7972c99c423
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-23 12:47:50 +0100

    about section one

--- src/components/about/section-one/section-one.js
+++ src/components/about/section-one/section-one.js
@@ -9,13 +9,9 @@ function SectionOne() {
         <img src={mountain} alt="mountain"></img>
         <h1 class="ap-s1-heading">About us</h1>
         <div class="ap-s1-text-body">
-        <div><h4>desserts/food/ramen/desserts</h4>
-        <h4>desserts/food/ramen/desserts</h4>
-        </div>
-          <div><h3>
-            asian fusion<hr></hr>
-          </h3>
-          </div>
+          <h2 class="h2-small">BLABLA</h2>
+          <h3>asian fusion</h3>
+          <hr></hr>
         </div>
       </div>
     </div>
