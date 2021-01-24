406fbba888d5f749ef74f029a80786ffdd2a891e
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-24 19:20:27 +0100

    content menu page done

--- src/components/menu/section-five-six/section-five-six.js
+++ src/components/menu/section-five-six/section-five-six.js
@@ -11,42 +11,36 @@ function SectionFiveAndSix() {
         </div>
         <img src={mountain} alt="mountain"></img>
         <div class="mp-s5-menu">
-          <h3>hello</h3>
+          <h3>All can be made alcohol free</h3>
           <div class="mp-s5-menu-text-wrapper-border">
             <div class="mp-s5-menu-text-wrapper">
               <h2 class="mp-s5-menu-heading">cocktails</h2>
               <div class="mp-s5-menu-text">
                 <div class="mp-s5-menu-text-col1">
+                  <h3>Fukushima Lightning</h3>
+                  <h5>Yuzusake, Cava, Yuzu salt</h5>
+                  <h4>115kr</h4>
+
+                  <h3>Pink Bloody Pink</h3>
                   <h5>
-                    "For the people, food is heaven. ” This is as true in
-                    tropical chili-loving Sichuan as among the hunters on the
-                    snow-capped peaks of the Himalayas. Likewise in the
-                    cumin-scented Muslim deserts on the Silk Road in Xinjiang or
-                    along the coasts with a seafood feast in the cities of
-                    Shanghai and Hong Kong. So let the journey begin! We want to
-                    recommend you to eat here with us in the same way as you do
-                    there! Which means that you order a bunch of dishes that you
-                    then eat together. We are more than happy to help you with
-                    recommendations on what should be eaten with what and how.
-                    It does not have to be more difficult than that. Warm
-                    welcome!
+                    Stockholms Bränneri Pink Gin, Blood Orange Umeshu, Pink
+                    Grapefruit
                   </h5>
+                  <h4>125kr</h4>
+                  <h3>Japaloma</h3>
+                  <h5>Tequila, Yuzusake, Pink Grapefruit, Yuzu salt, Soda</h5>
+                  <h4>145kr</h4>
                 </div>
                 <div class="mp-s5-menu-text-col2">
-                  <h5>
-                    "For the people, food is heaven. ” This is as true in
-                    tropical chili-loving Sichuan as among the hunters on the
-                    snow-capped peaks of the Himalayas. Likewise in the
-                    cumin-scented Muslim deserts on the Silk Road in Xinjiang or
-                    along the coasts with a seafood feast in the cities of
-                    Shanghai and Hong Kong. So let the journey begin! We want to
-                    recommend you to eat here with us in the same way as you do
-                    there! Which means that you order a bunch of dishes that you
-                    then eat together. We are more than happy to help you with
-                    recommendations on what should be eaten with what and how.
-                    It does not have to be more difficult than that. Warm
-                    welcome!
-                  </h5>
+                  <h3>Gin Toniku</h3>
+                  <h5>Gin, Yuzu, Organics Tonic Water</h5>
+                  <h4>125kr</h4>
+                  <h3>Cilantro Sour</h3>
+                  <h5>Gin, Lime, Sugar, Soda</h5>
+                  <h4>145kr</h4>
+                  <h3>Shu-groni</h3>
+                  <h5>Sake, Campari, Sweet vermouth</h5>
+                  <h4>155kr</h4>
                 </div>
               </div>
             </div>
@@ -56,42 +50,58 @@ function SectionFiveAndSix() {
       <div class="mp-s6-relative">
         <img src={mountain} alt="mountain"></img>
         <div class="mp-s6-menu">
-          <h3>hello</h3>
+          <h3>Chinese liquor and more</h3>
           <div class="mp-s6-menu-text-wrapper-border">
             <div class="mp-s6-menu-text-wrapper">
               <h2 class="mp-s6-menu-heading">other drinks</h2>
               <div class="mp-s6-menu-text">
                 <div class="mp-s6-menu-text-col1">
-                  <h5>
-                    "For the people, food is heaven. ” This is as true in
-                    tropical chili-loving Sichuan as among the hunters on the
-                    snow-capped peaks of the Himalayas. Likewise in the
-                    cumin-scented Muslim deserts on the Silk Road in Xinjiang or
-                    along the coasts with a seafood feast in the cities of
-                    Shanghai and Hong Kong. So let the journey begin! We want to
-                    recommend you to eat here with us in the same way as you do
-                    there! Which means that you order a bunch of dishes that you
-                    then eat together. We are more than happy to help you with
-                    recommendations on what should be eaten with what and how.
-                    It does not have to be more difficult than that. Warm
-                    welcome!
-                  </h5>
+                <h3>Baijiu / cl</h3>
+
+                  <h5>Red Star Classic 56%<br></br>
+                  Peking - Light
+                  aroma </h5>
+                  <h4>25kr</h4>
+
+                  <h5>Fenjiu 53%<br></br>
+                  Fenyang, Shanxi province - Light aroma</h5>
+                  <h4>28kr</h4>
+
+
+                  <h5>Touqu 52%<br></br>
+                  Luzhou, Sichuan province -Strong aroma</h5>
+                  <h4>29kr</h4>
+
+
+                  <h5>Wellbay 52%<br></br>
+                  Chengdu, Sichuan province -Strong aroma</h5>
+                  <h4>66kr</h4>
+
+
+                  <h5>Fenjiu 53%<br></br>
+                  Luzhou, Sichuan province -Strong aroma</h5>
+                  <h4>28kr</h4>
+                  
                 </div>
                 <div class="mp-s6-menu-text-col2">
-                  <h5>
-                    "For the people, food is heaven. ” This is as true in
-                    tropical chili-loving Sichuan as among the hunters on the
-                    snow-capped peaks of the Himalayas. Likewise in the
-                    cumin-scented Muslim deserts on the Silk Road in Xinjiang or
-                    along the coasts with a seafood feast in the cities of
-                    Shanghai and Hong Kong. So let the journey begin! We want to
-                    recommend you to eat here with us in the same way as you do
-                    there! Which means that you order a bunch of dishes that you
-                    then eat together. We are more than happy to help you with
-                    recommendations on what should be eaten with what and how.
-                    It does not have to be more difficult than that. Warm
-                    welcome!
-                  </h5>
+                  
+                  
+
+                  <h3>Shaoxing rice wine / 5cl</h3>
+                  <h5>Kuaijishan - 5 years, 14%</h5>
+                  <h4>75kr</h4>
+                  <h5>Kuaijishan - 8 years, 14%</h5>
+                  <h4>95kr</h4>
+                  <h5>Kuaijishan - 12 years, 14%
+            </h5>
+                  <h4>160kr</h4>
+                    
+                  <h3><br></br>Other drinks</h3>
+                  <h4>Mineral Water 25kr<br></br> 
+                  Light Beer 25kr<br></br> 
+                  Coca-Cola Zero 25kr<br></br> 
+                  Karma Cola 35kr<br></br> 
+                  Gingerella 35kr</h4>
                 </div>
               </div>
             </div>
--- src/components/menu/section-four/section-four.js
+++ src/components/menu/section-four/section-four.js
@@ -11,43 +11,34 @@ function SectionFour() {
           <h2 class="h2-small">Desserts</h2>
         </div>
         <div class="mp-s4-menu">
-          <h3>hello</h3>
+          <h3>The most important</h3>
           <div class="mp-s4-menu-text-wrapper-border">
             <div class="mp-s4-menu-text-wrapper">
               <h2 class="mp-s4-menu-heading">something</h2>
               <h2 class="mp-s4-menu-heading">sweet</h2>
               <div class="mp-s4-menu-text">
                 <div class="mp-s4-menu-text-col1">
-                  <h5>
-                    "For the people, food is heaven. ” This is as true in
-                    tropical chili-loving Sichuan as among the hunters on the
-                    snow-capped peaks of the Himalayas. Likewise in the
-                    cumin-scented Muslim deserts on the Silk Road in Xinjiang or
-                    along the coasts with a seafood feast in the cities of
-                    Shanghai and Hong Kong. So let the journey begin! We want to
-                    recommend you to eat here with us in the same way as you do
-                    there! Which means that you order a bunch of dishes that you
-                    then eat together. We are more than happy to help you with
-                    recommendations on what should be eaten with what and how.
-                    It does not have to be more difficult than that. Warm
-                    welcome!
-                  </h5>
+                  <h3>Bignè</h3>
+                  <h5>Red bean mousse and strawberry</h5>
+                  <h4>85kr</h4>
+                  <h3>COCONUT CRÈME BRÛLÉE</h3>
+                  <h5>With nashi pear sorbet</h5>
+                  <h4>85kr</h4>
+                  <h3>Wet cake</h3>
+                  <h5>Green papaya, lime and coconut</h5>
+                  <h4>120kr</h4>
                 </div>
                 <div class="mp-s4-menu-text-col2">
+                  <h3>THe icecream</h3>
                   <h5>
-                    "For the people, food is heaven. ” This is as true in
-                    tropical chili-loving Sichuan as among the hunters on the
-                    snow-capped peaks of the Himalayas. Likewise in the
-                    cumin-scented Muslim deserts on the Silk Road in Xinjiang or
-                    along the coasts with a seafood feast in the cities of
-                    Shanghai and Hong Kong. So let the journey begin! We want to
-                    recommend you to eat here with us in the same way as you do
-                    there! Which means that you order a bunch of dishes that you
-                    then eat together. We are more than happy to help you with
-                    recommendations on what should be eaten with what and how.
-                    It does not have to be more difficult than that. Warm
-                    welcome!
+                    Warm chocolate chawanmushi, white miso, maple syrup and
+                    cherry yogurt ice cream
                   </h5>
+                  <h4>115kr</h4>
+
+                  <h3>Apple Pie (Dessert drink)</h3>
+                  <h5>Secret recipe!</h5>
+                  <h4>145kr</h4>
                 </div>
               </div>
             </div>
