0023b6398d7db2cf1363b54f07f624bf9e266253
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-01-26 19:36:44 +0100

    commit

--- src/components/menu/section-four/section-four.js
+++ src/components/menu/section-four/section-four.js
@@ -12,34 +12,33 @@ function SectionFour() {
         </div>
         <div class="mp-s4-menu">
           <h3>The most important</h3>
-          
-            <div class="mp-s4-menu-text-wrapper">
-              <h2 class="mp-s4-menu-heading">something</h2>
-              <h2 class="mp-s4-menu-heading">sweet</h2>
-              <div class="mp-s4-menu-text">
-                <div class="mp-s4-menu-text-col1">
-                  <h3>Bignè</h3>
-                  <h5>Red bean mousse and strawberry</h5>
-                  <h4>85kr</h4>
-                  <h3>COCONUT CRÈME BRÛLÉE</h3>
-                  <h5>With nashi pear sorbet</h5>
-                  <h4>85kr</h4>
-                  <h3>Wet cake</h3>
-                  <h5>Green papaya, lime and coconut</h5>
-                  <h4>120kr</h4>
-                </div>
-                <div class="mp-s4-menu-text-col2">
-                  <h3>THe icecream</h3>
-                  <h5>
-                    Warm chocolate chawanmushi, white miso, maple syrup and
-                    cherry yogurt ice cream
-                  </h5>
-                  <h4>115kr</h4>
 
-                  <h3>Apple Pie (Dessert drink)</h3>
-                  <h5>Secret recipe!</h5>
-                  <h4>145kr</h4>
-                
+          <div class="mp-s4-menu-text-wrapper">
+            <h2 class="mp-s4-menu-heading">something</h2>
+            <h2 class="mp-s4-menu-heading">sweet</h2>
+            <div class="mp-s4-menu-text">
+              <div class="mp-s4-menu-text-col1">
+                <h3>Bignè</h3>
+                <h5>Red bean mousse and strawberry</h5>
+                <h4>85kr</h4>
+                <h3>COCONUT CRÈME BRÛLÉE</h3>
+                <h5>With nashi pear sorbet</h5>
+                <h4>85kr</h4>
+                <h3>Wet cake</h3>
+                <h5>Green papaya, lime and coconut</h5>
+                <h4>120kr</h4>
+              </div>
+              <div class="mp-s4-menu-text-col2">
+                <h3>THe icecream</h3>
+                <h5>
+                  Warm chocolate chawanmushi, white miso, maple syrup and cherry
+                  yogurt ice cream
+                </h5>
+                <h4>115kr</h4>
+
+                <h3>Apple Pie (Dessert drink)</h3>
+                <h5>Secret recipe!</h5>
+                <h4>145kr</h4>
               </div>
             </div>
           </div>
